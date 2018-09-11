c188fbf58dd4a3efe845f58e010029aa6fc12fae
meduardacn <maria.nascimento.002@acad.pucrs.br>
2018-09-11 11:27:05 -0300

    11/09

--- src/main/java/com/bcopstein/Numerologia/IFormatador.java
+++ src/main/java/com/bcopstein/Numerologia/IFormatador.java
@@ -2,4 +2,6 @@ package com.bcopstein.Numerologia;
 
 public interface IFormatador {
 	public String formataFrase(String frase);
+    public String formataPalavra(String palavra);    
+    public String formataPalavraPlus(String palavra);
 }
--- src/main/java/com/bcopstein/Numerologia/IRedutor.java
+++ src/main/java/com/bcopstein/Numerologia/IRedutor.java
@@ -3,4 +3,7 @@ package com.bcopstein.Numerologia;
 public interface IRedutor {
 	public int reducaoData(String data);
 	public int reducaoFrase(String frase);
+	public void setTipoTabela(TipoTabela tipo);
+    public int reducaoPalavra(String palavra);
+   
 }
